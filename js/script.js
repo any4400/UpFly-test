0744494f6fd47b0b63d4d496d4ca54a9db5501b5
Anna Shukova <any4400@yandex.ru>
2023-02-22 01:44:41 +0300

    Добавлена возможность закрывания модального окна и меню кликом по свободному пространству, изменены крестики для закрытия модального окна и меню, улучшено наведение на иконки соц.сетей

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-	$('.imdiz-open').click(function(){
+	$('.modal-open').click(function(){
 		$('.overlay').css({'visibility': 'visible', 'opacity': 1});
 		$('.imdiz-modal').css({'visibility': 'visible', 'opacity': 1});
 	});
-	$('.imdiz-close').click(function(){
+	$('.modal-close').click(function(){
 		$('.overlay').css({'visibility': 'hidden', 'opacity': 0});
 		$('.imdiz-modal').css({'visibility': 'hidden', 'opacity': 0});
 	});
-	$('.burger-open').click(function(){
+	$('.burger-menu-open').click(function(){
 		$('.overlay').css({'visibility': 'visible', 'opacity': 1});
 		$('.menu').css({'visibility': 'visible', 'opacity': 1});
 	});
-	$('.burger-close').click(function(){
+	$('.burger-menu-close').click(function(){
 		$('.overlay').css({'visibility': 'hidden', 'opacity': 0});
 		$('.menu').css({'visibility': 'hidden', 'opacity': 0});
 	});
