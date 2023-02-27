e6179480c2d62f4350a64539b4552b9c7c4948ef
Anna Shukova <any4400@yandex.ru>
2023-02-27 17:39:24 +0300

    применены новые способы организации CSS-кода: SMACSS и Atomic CSS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-	$('.modal-open').click(function(){
+	$('.modal-open-js').click(function(){
 		$('.overlay').css({'visibility': 'visible', 'opacity': 1});
 		$('.imdiz-modal').css({'visibility': 'visible', 'opacity': 1});
 	});
-	$('.modal-close').click(function(){
+	$('.modal-close-js').click(function(){
 		$('.overlay').css({'visibility': 'hidden', 'opacity': 0});
 		$('.imdiz-modal').css({'visibility': 'hidden', 'opacity': 0});
 	});
-	$('.burger-menu-open').click(function(){
+	$('.burger-menu-open-js').click(function(){
 		$('.overlay').css({'visibility': 'visible', 'opacity': 1});
 		$('.menu').css({'visibility': 'visible', 'opacity': 1});
 	});
-	$('.burger-menu-close').click(function(){
+	$('.burger-menu-close-js').click(function(){
 		$('.overlay').css({'visibility': 'hidden', 'opacity': 0});
 		$('.menu').css({'visibility': 'hidden', 'opacity': 0});
 	});
